1933c1d7e937d142432f39e701eb14176e45fd22
Kenneth Chen <kenchen0606@gmail.com>
2021-11-08 22:41:41 +0000

    added test pages

--- express_server.js
+++ express_server.js
@@ -11,9 +11,18 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-// app.get("/test", (req, res) => {
-//   res.send("test page!");
-// });
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+});
+
+app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`); 
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
