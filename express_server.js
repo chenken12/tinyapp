5746d53588a7772303caceeb31f253c4acf095df
Kenneth Chen <kenchen0606@gmail.com>
2021-11-09 03:08:04 +0000

    redirect

--- express_server.js
+++ express_server.js
@@ -20,15 +20,6 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
-
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`); 
-});
-
 app.get("/hello", (req, res) => {
   const templateVars = { greeting: 'Hello World!' };
   res.render("hello_world", templateVars);
@@ -38,7 +29,9 @@ app.post("/urls", (req, res) => {
   const r = generateRandomString();
   //console.log("random", r);
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  //res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  urlDatabase[r] = req.body.longURL;
+  res.render("urls_show", {shortURL:r, longURL:urlDatabase[r]});
 });
 
 app.get("/urls", (req, res) => {
@@ -56,6 +49,10 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
